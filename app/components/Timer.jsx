28557d887b065396de4d0227afc799fd360b3ee3
Ahmad Malik <121057088+Malik-AhmadSE@users.noreply.github.com>
2024-01-09 16:58:31 +0500

    all set

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -28,7 +28,7 @@ export default function Timer() {
 
   return (
     <div className="w-[260px] h-[90px] md:w-[390px] md:h-[210px] bg-yellow-400 rounded-xl shadow-sm">
-      <h1 className="flex justify-center pt-2 font-bold text-[15px] md:text-[25px] text-black">AI-Real Time Signal Machine</h1>
+      <h1 className="flex justify-center pt-2 font-bold text-[15px] md:text-[25px] text-black">AI Real-Time Signal Machine</h1>
       <div className="w-full h-full flex justify-center items-center">
         <h2 className=" text-black font-semibold text-[12px] mb-10 md:text-[24px]">{time}</h2>
       </div>
